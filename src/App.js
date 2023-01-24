0f8f2c7d6a0fa11abc513ef613115bce3f76376d
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2023-01-24 13:02:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './index.css'
+import api from './services/api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({})
 
+  async function handleSearch(){
+    // 01310930/json/
+    if(input === ''){
+      alert('Erro: Preencha com um CEP válido e somente com números');
+       input('');
+       return;
+
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    } catch{
+      alert('Erro ao consultar')
+      setInput('')
+    }
+     
+  }
+  return (
+    <div className="container">
+      <h1 className="container-title">Consultar CEP</h1>
+      <div className="containerInput">
+        <input
+          type="number"
+          placeholder="Somente números"
+          value={input}
+          maxLength='8'
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF"></FiSearch>
+        </button>
+      </div>
+
+  {Object.keys(cep).length > 0 && (
+      <main className="main">
+        <h2 className="title-cep">CEP: {cep.cep}</h2>  
+        <span className="span-info">Rua: {cep.logradouro}</span>
+        <span className="span-info">Complemento: {cep.complemento}</span>
+        <span className="span-info">Bairro: {cep.bairro}</span>
+        <span className="span-info">Localidade: {cep.localidade} - {cep.uf}</span>
+        <span className="span-info">DDD: {cep.ddd}</span>
+      </main>
+  )}
+
+    </div>
+  );  
+}
 export default App;
